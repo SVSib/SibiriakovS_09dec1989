e491a9d258da4e0b8ad8458ea9b2d54d39402737
Сергей <typesib@gmail.com>
2025-05-01 15:56:43 +0700

    second_commit_auto

--- Object/object_ui.py
+++ Object/object_ui.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -33,9 +34,10 @@ class Object_UI:
                 Удаление товара из корзины
         """
 
-        delete_prod = self._driver.find_element(By.CSS_SELECTOR, ".basket__delete.js-item-delete")
+        delete_prod = self._driver.find_element(By.CSS_SELECTOR, "div[class='basket__delete js-item-delete']")
         delete_prod.click()
-
+        WebDriverWait(self._driver, 10).until_not(EC.visibility_of_element_located(
+        (By.XPATH, "div[class='basket__delete js-item-delete']")))
 
     def enter_to_cart(self):
         """
